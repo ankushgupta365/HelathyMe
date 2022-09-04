a6cbe640734e9931b73c737fc0c02d49e4378a12
Ankush Gupta <ankushgupta365@gmail.com>
2022-09-04 13:00:40 +0530

    nutrional values added

--- src/components/home/Demo.js
+++ src/components/home/Demo.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from "axios"
 import doctor from './doctor.png'
+import { Button, Modal } from 'antd';
 // import { FileImageOutlined } from '@ant-design/icons';
 // import { Input, Space } from 'antd';
 // const { Search } = Input;
@@ -12,8 +13,9 @@ const Demo = () => {
   const [image, setImage] = useState('')
   const [imageId, setImageId] = useState(null)
   const [dishName, setDishName] = useState(null)
-  const [img,setImg] = useState(null)
-
+  const [img, setImg] = useState(null)
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [nutriData, setNutriData] = useState(null)
 
   const handleChange = (e) => {
     console.log(e.target.files)
@@ -31,7 +33,7 @@ const Demo = () => {
       method: 'POST',
       url: 'https://api.logmeal.es/v2/recognition/dish',
       headers: {
-        'Authorization': 'Bearer 87e9cf703d15e4f1e72c1de2df4aeabaca36c56f',
+        'Authorization': 'Bearer 8d46848d01dd3ea3a29bc4a59f5b3008c3f83e0d',
       },
       data: formData
     };
@@ -44,45 +46,74 @@ const Demo = () => {
     });
   }
 
-  const getNutriInfo=()=>{
-    const idimg = {'imageId': imageId}
+  const getNutriInfo = () => {
+    const idimg = { 'imageId': imageId }
     const options = {
       method: 'POST',
       url: 'https://api.logmeal.es/v2/nutrition/recipe/nutritionalInfo',
       headers: {
-        'Authorization': 'Bearer 87e9cf703d15e4f1e72c1de2df4aeabaca36c56f',
+        'Authorization': 'Bearer 8d46848d01dd3ea3a29bc4a59f5b3008c3f83e0d',
       },
       data: idimg
     };
     axios.request(options).then(function (response) {
-      console.log(response.data);
-      // setImageId(response.data.imageId)
-      // setDishName(response.data.recognition_results[0].name)
+      console.log(response.data)
+      setNutriData(response.data);
+      // console.log(response.data.nutritional_info.calories)
+      // console.log("carbs",response.data.nutritional_info.dailyIntakeReference['CHOCDF'].level)
+      // console.log("saturated fat",response.data.nutritional_info.dailyIntakeReference['FASAT'].level)
+      // console.log("fat",response.data.nutritional_info.dailyIntakeReference['FAT'].level)
+      // console.log("protein",response.data.nutritional_info.dailyIntakeReference['PROCNT'].level)
+      // console.log("sugar",response.data.nutritional_info.dailyIntakeReference['SUGAR'].level)
+
+      // console.log(response.data.nutriData.nutritional_info.calories)
     }).catch(function (error) {
       console.error(error);
     });
   }
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
-    <section className='container'>
-      <div className='child parent-child'>
-        <h2>
-          One step solution to all your dietary needs
-        </h2>
-        <div style={{ display: 'flex', marginTop: '20px' }}>
-          <input type="file" onChange={handleChange} />
-          <button onClick={handleApi} >SUBMIT</button>
+    <>
+      <section className='container'>
+        <div className='child parent-child'>
+          <h2>
+            One step solution to all your dietary needs
+          </h2>
+          <div style={{ display: 'flex', marginTop: '20px' }}>
+            <input type="file" onChange={handleChange} />
+            <button onClick={handleApi} >SUBMIT</button>
+          </div>
+          {imageId && dishName && img && <div style={{ marginTop: '20px' }}>
+            <img src={img} widht="120" height="180" />
+            <p>{dishName}</p>
+            <button onClick={getNutriInfo} >Get Nutri info</button>
+          </div>}
         </div>
-      </div>
-      <div className='child'>
-        <img src={doctor} alt="athlete" width="550" height="750"></img>
-      </div>
-      {imageId && dishName && img && <div>
-        {/* <p>{imageId}</p> */}
-        <img src={img} widht="120" height="180"/>
-        <p>{dishName}</p>
-        <button onClick={getNutriInfo} >Get Nutri info</button>
-      </div>}
-    </section>
+        {nutriData &&
+          <div className='child'>
+            <p>calories: {nutriData.nutritional_info.calories}</p>
+            <p>carbs: {nutriData.nutritional_info.dailyIntakeReference['CHOCDF'].level}</p>
+            <p>Saturated fats: {nutriData.nutritional_info.dailyIntakeReference['FASAT'].level}</p>
+            <p>protein: {nutriData.nutritional_info.dailyIntakeReference['PROCNT'].level}</p>
+            <p>sugar: {nutriData.nutritional_info.dailyIntakeReference['SUGAR'].level}</p>
+          </div>}
+        <div className='child'>
+          <img src={doctor} alt="athlete" width="550" height="750"></img>
+        </div>
+      </section>
+    </>
   )
 }
 
